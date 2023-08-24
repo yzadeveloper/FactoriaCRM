d1d7fceeb1301bd5647ffbc56af9ccc4db590455
Maggie <maggie.filgueira@gmail.com>
2023-08-24 11:06:23 +0200

    update create

--- src/views/candidato/create.php
+++ src/views/candidato/create.php
@@ -53,7 +53,7 @@
                 
                 <br><br>
             <button type="submit" class="btn btn-primary">Guardar</button>
-            <a class="btn btn-danger" href="../../index.php">Cancelar</a>
+            <a class="btn btn-danger" href="../../../index.php">Cancelar</a>
             <br><br>
         </form>
     </div>
